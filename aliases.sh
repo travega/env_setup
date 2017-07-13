7acecc7ac246981d6321b8e8954f2ef8724f4f20
Andy Scott <travega@gmail.com>
2017-07-13 22:50:54 +0100

    shell aliases

---
+++ aliases.sh
@@ -0,0 +1,17 @@
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias ga='git add'
+alias gs='git status'
+alias gco='git checkout'
+alias gr='git rebase'
+alias gpush='git push'
+alias gpull='git pull'
+alias gl='git log'
+alias ..='cd ..'
+alias grm='git rm --cache'
+alias apt='sudo apt-get'
+alias src='~/src; ll'
